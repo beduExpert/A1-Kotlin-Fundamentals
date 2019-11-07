8453f6865b6b1c45e6060c652759a77d5a71c731
Alejandro Isai Acosta Martinez <isaiin.dev@gmail.com>
2019-11-07 10:14:14 -0600

    Solucion reto 1

--- Sesion-01/Reto-01/src/solucion.kt
+++ Sesion-01/Reto-01/src/solucion.kt
@@ -41,5 +41,12 @@ fun main(args: Array<String>) {
     println(a % b)
     println(a ..b)
 
+    println(a.plus(b))
+    println(a.minus(b))
+    println(a.times(b))
+    println(a.div(b))
+    println(a.rem(b))
+    println(a.rangeTo(b))
+
 
 }
